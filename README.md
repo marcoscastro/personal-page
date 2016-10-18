f790543ca7d1afc24e7fe900681fa325e713bc08
Marcos Castro <mcastrosouza@live.com>
2016-10-18 18:55:17 -0300

    added requierements

---
+++ README.md
@@ -0,0 +1,6 @@
+# My personal website
+My personal page made with Python language and Bottle (web-framework for Python).
+
+Python + Bottle = happiness ;-)
+
+You can use and do what you want! Doubts? mcastrosouza@live.com
---
+++ app.py
@@ -0,0 +1,54 @@
+from bottle import route, run
+from bottle import static_file, request
+from bottle import template, get, error
+
+DEBUG = False
+
+# static routes
+@get('/<filename:re:.*\.css>')
+def stylesheets(filename):
+	return static_file(filename, root='static/css')
+
+@get('/<filename:re:.*\.js>')
+def javascripts(filename):
+	return static_file(filename, root='static/js')
+
+@get('/<filename:re:.*\.(jpg|png|gif|ico)>')
+def images(filename):
+	return static_file(filename, root='static/img')
+
+@get('/<filename:re:.*\.(eot|ttf|woff|svg)>')
+def fonts(filename):
+	return static_file(filename, root='static/fonts')
+
+@route('/')
+def index():
+	return template('index')
+
+@route('/skills')
+def skills():
+	return template('skills')
+
+@route('/about')
+def about():
+	return template('about')
+
+@route('/courses')
+def courses():
+	return template('courses')
+
+@route('/contact', method='POST')
+def acao_login():
+	name = request.forms.get('name')
+	email = request.forms.get('email')
+	site = request.forms.get('site')
+	message = request.forms.get('message')
+	print(message)
+	return template('contact', name=name)
+
+@error(404)
+def error404(error):
+	return template('oops')
+
+if __name__ == "__main__":
+	run(host='localhost', port=8080, reloader=DEBUG, debug=DEBUG)
