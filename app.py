58dfa1e8df073cafc23871e76d317172758b05a6
Marcos Castro <mcastrosouza@live.com>
2016-10-21 01:54:39 -0300

    change app.py

--- app.py
+++ app.py
@@ -53,5 +53,4 @@ if __name__ == "__main__":
 	if os.environ.get('APP_LOCATION') == 'heroku':
 		run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
 	else:
-		print('ENTROU AQUI')
 		run(host='localhost', port=8080, debug=True, reloader=True)
